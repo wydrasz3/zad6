853bd6bb9f21464bc4050323d1cef2ac85bf3fa0
MSI <wydrasz3@gmail.com>
2020-02-15 15:47:10 +0100

    kontroler

--- src/main/java/pl/sda/zad6/controller/MainController.java
+++ src/main/java/pl/sda/zad6/controller/MainController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -12,15 +11,15 @@ public class MainController {
     public static final String PAGE_NAME = "index";
 
     @GetMapping
-    public String page(@RequestParam(value = "button1", required = false) String clickedButton, Model model) {
-        if (clickedButton != null) {
-            model.addAttribute("mojAtrybut", "Dane z serwera");
-        }
+    public String page(Model model) {
+        model.addAttribute("mojAtrybut", "");
         return PAGE_NAME;
     }
 
-//    @PostMapping
-//    public String page(@RequestParam("button1") String button, Model model) {
-//        return PAGE_NAME;
-//    }
+    @PostMapping
+    public String postPage(Model model) {
+        model.addAttribute("mojAtrybut", "Dane z serwera");
+        return PAGE_NAME;
+    }
+
 }
